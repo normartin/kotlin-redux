943efe5f1c38a7aede07443c4bef016e82d50ea0
martinmoeller <martinmoeller>
2020-02-01 23:03:35 +0100

    init

---
+++ src/test/java/store/redux/tools.kt
@@ -0,0 +1,10 @@
+package store.redux
+
+fun <E> List<E>.update(index: Int, f: ((E) -> E)): List<E> = this.mapIndexed { i, e ->
+    if (i == index) {
+        f(e)
+    } else {
+        e
+    }
+}
+
